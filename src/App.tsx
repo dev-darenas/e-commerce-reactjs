d54f6370aad0af2ba14018e2a97eadb528f8233f
Daniel Arenas <darenas@softdremas.co>
2020-06-22 22:26:58 -0500

    Added coming soon screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+
+const slider = (
+  <AwesomeSlider 
+    animation="foldOutAnimation"
+  >
+    <div data-src="/img/laia_1.jpeg">
+      <h2>Hola!!</h2>
+    </div>
+    <div data-src="/img/laia_2.jpeg" />
+    <div data-src="/img/laia_5.jpeg" />
+  </AwesomeSlider>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <div></div>
   );
 }
 
